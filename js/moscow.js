9d364d7389d29a6a5f01fe3e1fa523e98c159600
Sergei Tundykov <pcms2@bk.ru>
2022-10-08 22:32:53 +0300

    New site

--- js/moscow.js
+++ js/moscow.js
@@ -1,10 +1,10 @@
 function itogovoe_sochineniye_2022_11klass_alert() {
-  let text =prompt("Вы зарегестрировались на итоговое сочинение\nЧто хотите сказать?", "");
+  let text =prompt("Вы успешно зарегестрировались на итоговое сочинение!\nЧто хотите сказать?", "");
   var audio = new Audio();
   audio.src = "img/gimn.mp3";
   audio.autoplay = true;
   
-  while(text != "Как похорошела москва при Сёргее Семеновиче!" && text.toLowerCase() != "кукареку") {
+  while(text != "Как похорошела москва при Сёргее Семеновиче!" && text.toLowerCase() != "кукареку" && text.toLowerCase() != "паша тЕхник жид") {
     text = prompt("Неправильно! повторите ещо\nЧто хотите сказать?", "")
   }
   audio.pause()
