e97bcfeb2dc6c667d30e9b413d3e1c5345b9ba95
Sergei Tundykov <pcms2@bk.ru>
2022-10-08 13:02:08 +0300

    New site

--- js/genMer.js
+++ js/genMer.js
@@ -1,33 +1,54 @@
-function getRandomInt(max) {
+let ads120x500Items = [
+  {
+    image: "/img/mer120x500/1.png",
+    link: "https://mos.ru"
+  },
+  {
+    image: "/img/mer120x500/2.png",
+    link: "https://mos.ru"
+  },
+  {
+    image: "/img/mer120x500/3.png",
+    link: "https://mos.ru"
+  },
+  {
+    image: "/img/mer120x500/4.png",
+    link: "https://mos.ru"
+  },
+  {
+    image: "/img/mer120x500/5.png",
+    link: "https://rlc.education"
+  },
+  {
+    image: "/img/mer120x500/6.png",
+    link: "https://mos.ru"
+  },
+  {
+    image: "/img/mer120x500/7.png",
+    link: "https://mos.ru"
+  }
+];
+
+
+function randint(max) {
   return Math.floor(Math.random() * max);
 }
-
-function loadFile(filePath) {
-  var result = null;
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open('GET', filePath, false);
-  xmlhttp.send();
-  if (xmlhttp.status==200) {
-    result = xmlhttp.responseText;
+const leftMerBlock = 2;
+for(let i = 1; i <= leftMerBlock; i++) {
+  let j = randint(ads120x500Items.length);
+  let textBanner;
+  if(i == leftMerBlock) {
+    textBanner = `<a id="mer120x500_${i}" href="${ads120x500Items[j].link}" class="mer120x500">
+      <img id="mer120x500IMG_${i}" class="mer120x500IMG" src="${ads120x500Items[j].image}">
+    </a>
+    `;
+  } else {
+    textBanner = `<a id="mer120x500_${i}" href="${ads120x500Items[j].link}" class="mer120x500" style="padding-bottom:32px;">
+      <img id="mer120x500IMG_${i}" class="mer120x500IMG" src="${ads120x500Items[j].image}">
+    </a>
+    `;
   }
-  return result;
+
+  let banner = document.querySelector('#contentLeft');
+  banner.insertAdjacentHTML("afterbegin", textBanner);
 }
-
-let b = [];
-let zzzz = 1;
-while(1) {
-  let res = loadFile(`/img/mer120x500/${zzzz}.png`)
-  if(res == null) {
-    break;
-  }
-  b.push(`/img/mer120x500/${zzzz}.png`);
-  zzzz++;
-}
-
-
-let textBanner = `
-<img src="${b[getRandomInt(b.length)]}">
-`
-
-let banner = document.querySelector('#mer120x500')
-banner.insertAdjacentHTML("afterbegin", textBanner)
